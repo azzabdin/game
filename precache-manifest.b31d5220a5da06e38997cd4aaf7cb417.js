c5ba35119d1406389d0ab47c2b21eb6b7cf6f90e
azza <azzabdin@gmail.com>
2019-06-22 21:21:14 -0400

    Updates

---
+++ precache-manifest.b31d5220a5da06e38997cd4aaf7cb417.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a5cca39ecf97e54f68ad34082ac689",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "c4de83899a1ff433dea0",
+    "url": "/game/static/css/main.52b76a77.chunk.css"
+  },
+  {
+    "revision": "c65f0670c7d7a243f52e",
+    "url": "/game/static/js/2.f75e8bbc.chunk.js"
+  },
+  {
+    "revision": "c4de83899a1ff433dea0",
+    "url": "/game/static/js/main.b36400e5.chunk.js"
+  },
+  {
+    "revision": "bab8f84a0f4d6b7ca58b",
+    "url": "/game/static/js/runtime~main.2d0fbc73.js"
+  }
+]);
